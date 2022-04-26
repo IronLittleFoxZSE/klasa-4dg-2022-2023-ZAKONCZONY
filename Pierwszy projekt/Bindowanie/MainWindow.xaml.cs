7bd43e1e984299cbf0e90cf3adb077cfc7e695b1
Łukasz Felisek <felisek1@wp.pl>
2022-04-26 11:39:14 +0200

    koniec lekcji

--- Pierwszy projekt/Bindowanie/MainWindow.xaml.cs
+++ Pierwszy projekt/Bindowanie/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,14 +14,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Bindowanie.Annotations;
 
 namespace Bindowanie
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public string DanaTextBox { get; set; }
 
         public string Dana_z_widoku_do_kodu { get; set; }
@@ -28,6 +33,11 @@ namespace Bindowanie
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
@@ -40,5 +50,25 @@ namespace Bindowanie
         {
             MessageBox.Show(DanaTextBox);
         }
-    }
+
+        private void ButtonBase2_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(Dana_z_widoku_do_kodu, out int liczba))
+            {
+                liczba = liczba * 2;
+                Dana_z_kodu_do_widoku = liczba.ToString();
+                //Dana_z_kodu_do_widoku
+                OnPropertyChanged("Dana_z_kodu_do_widoku");
+            }
+            else
+            {
+                Dana_z_kodu_do_widoku = "Podano wartość nie będącą liczbą!!!!";
+                OnPropertyChanged(nameof(Dana_z_kodu_do_widoku));
+            }
+
+        }
+
+
+        
+        
 }
