d6c7fb037f5637d34788701e9318f1005a6b575f
Łukasz Felisek <felisek1@wp.pl>
2022-04-28 09:50:22 +0200

    koniec lekcji

---
+++ Pierwszy projekt/Konwertery/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Konwertery.Annotations;
+
+namespace Konwertery
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Visibility _widocznoscV1;
+        public Visibility WidocznoscV1
+        {
+            get
+            {
+                return _widocznoscV1;
+            }
+            set
+            {
+                _widocznoscV1 = value;
+                OnPropertyChanged(nameof(WidocznoscV1));
+            }
+        }
+
+        private bool _widocznoscV2;
+        public bool WidocznoscV2
+        {
+            get
+            {
+                return _widocznoscV2;
+            }
+            set
+            {
+                _widocznoscV2 = value;
+                OnPropertyChanged(nameof(WidocznoscV2));
+            }
+        }
+
+        private bool _czyZazanczony;
+        public bool CzyZazanczony
+        {
+            get
+            {
+                return _czyZazanczony;
+            }
+            set
+            {
+                _czyZazanczony = value;
+                OnPropertyChanged(nameof(CzyZazanczony));
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void ButtonUkryjV1_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (WidocznoscV1 == Visibility.Visible)
+            {
+                WidocznoscV1 = Visibility.Hidden;
+            }
+            else
+            {
+                WidocznoscV1 = Visibility.Visible;
+            }
+        }
+
+        private void ButtonUkryjV2_OnClick(object sender, RoutedEventArgs e)
+        {
+            WidocznoscV2 = !WidocznoscV2;
+        }
+    }
+}
---
+++ Pierwszy projekt/Projekt comboBox/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Projekt_comboBox.Annotations;
+
+namespace Projekt_comboBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public List<string> ListaPozycji { get; set; }
+
+        public List<KolorNaLiscie> ListaKolorow { get; set; }
+
+        public KolorNaLiscie WybranyKolor { get; set; }
+
+        public bool AktywujPrzycisk
+        {
+            get
+            {
+                return WybranyKolor != null;
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListaPozycji = new List<string>();
+            ListaPozycji.Add("Pozycja bindowania 0");
+            ListaPozycji.Add("Pozycja bindowania 1");
+            ListaPozycji.Add("Pozycja bindowania 2");
+            OnPropertyChanged(nameof(ListaPozycji));
+
+            ListaKolorow = new List<KolorNaLiscie>();
+            ListaKolorow.Add(new KolorNaLiscie()
+            {
+                NazwaKoloru = "Green",
+                OpisKoloru = "Zielony"
+            });
+            ListaKolorow.Add(new KolorNaLiscie()
+            {
+                NazwaKoloru = "Red",
+                OpisKoloru = "Czerwony"
+            });
+            OnPropertyChanged(nameof(ListaKolorow));
+            OnPropertyChanged(nameof(AktywujPrzycisk));
+
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            string tekst = "wybrano kolor: ";
+            if (WybranyKolor != null)
+                tekst = tekst + WybranyKolor.OpisKoloru;
+            else
+                tekst = tekst + "Brak koloru";
+            MessageBox.Show(tekst);
+        }
+    }
+}
