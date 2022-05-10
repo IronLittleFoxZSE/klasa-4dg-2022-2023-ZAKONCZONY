af35717d2a3d3a71c250b22f584fa0fa74cce759
Łukasz Felisek <felisek1@wp.pl>
2022-05-10 11:40:25 +0200

    koniec lekcji

---
+++ Pierwszy projekt/Konwertery/NaszKonwertery/BoolToDescriptionConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Konwertery.NaszKonwertery
+{
+    class BoolToDescriptionConverter: IValueConverter
+    {
+        //z kodu/właściwości do widoku
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                bool wartosc = (bool) value;
+                return wartosc ? "Kontrolka zaznaczona" : "Kontrolka odznaczona";
+            }
+
+            return Binding.DoNothing;
+        }
+
+        //z widoku do kodu/właściwości
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
