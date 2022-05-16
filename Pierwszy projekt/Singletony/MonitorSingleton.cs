9a990bb080b254f911c967e0ea7ad85fc2064903
Łukasz Felisek <felisek1@wp.pl>
2022-05-16 12:30:39 +0200

    koniec lekcji

---
+++ Pierwszy projekt/Singletony/MonitorSingleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singletony
+{
+    class MonitorSingleton
+    {
+        private static MonitorSingleton instancja = null;
+
+        public static MonitorSingleton ZwrocObiektSingletonu()
+        {
+            if (instancja == null)
+                instancja = new MonitorSingleton();
+
+            return instancja;
+        }
+
+        public string NazwaMonitora { get; set; }
+
+        private MonitorSingleton()
+        {
+            
+        }
+
+        public void WyswietlNazwe()
+        {
+            Console.WriteLine(NazwaMonitora);
+        }
+
+    }
+}
---
+++ Pierwszy projekt/Singletony/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Singletony
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MonitorSingleton m1;// = new MonitorSingleton();
+            m1 = MonitorSingleton.ZwrocObiektSingletonu();
+
+            m1.NazwaMonitora = "AOC";
+
+            MonitorSingleton m2 = MonitorSingleton.ZwrocObiektSingletonu();
+            m2.WyswietlNazwe();
+        }
+    }
+}
