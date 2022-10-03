18c7939b5218b73b5b56fe88dd71af0c765c52b4
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-10-03 12:29:50 +0200

    eeaeds

---
+++ Pierwszy projekt/CarouselPageApp/CarouselPageApp/Model/PhotoInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarouselPageApp.Model
+{
+    class PhotoInfo
+    {
+        public string Name { get; set; }
+        public string PhotoUrl { get; set; }
+    }
+}
---
+++ Pierwszy projekt/CarouselPageApp/CarouselPageApp/ViewModel/PhotoInfoViewModel.cs
@@ -0,0 +1,36 @@
+﻿using CarouselPageApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarouselPageApp.ViewModel
+{
+    class PhotoInfoViewModel
+    {
+        public List<PhotoInfo> ListOfPhoto { get; set; }
+
+        public PhotoInfoViewModel()
+        {
+            ListOfPhoto = new List<PhotoInfo>();
+
+            ListOfPhoto.Add(new PhotoInfo()
+            {
+                Name = "Pies",
+                PhotoUrl = "https://img.freepik.com/darmowe-wektory/zmeczony-mlody-pies_1308-25268.jpg"
+            });
+
+            ListOfPhoto.Add(new PhotoInfo()
+            {
+                Name = "Piesek",
+                PhotoUrl = "https://img.freepik.com/darmowe-wektory/ladny-pies-ugryzienie-kosci-kreskowka-wektor-ikona-ilustracja-koncepcja-ikona-zywnosci-zwierzat-na-bialym-tle-premium-wektor-plaski-styl-kreskowki_138676-4161.jpg"
+            });
+
+            ListOfPhoto.Add(new PhotoInfo()
+            {
+                Name = "Kotki",
+                PhotoUrl = "https://img.freepik.com/darmowe-wektory/ladny-kot-i-pies-corgi-kreskowka-wektor-ikona-ilustracja-koncepcja-ikona-przyjaciela-zwierzat-na-bialym-tle-premium-wektor-plaski-styl-kreskowki_138676-3558.jpg"
+            });
+        }
+
+    }
+}
