b56de18d00b14702ae4c5977c3df2b6955fc03be
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-11-29 13:01:21 +0100

    dodanie zadania

---
+++ Pierwszy projekt/CaesarCode/CaesarCodeClass.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace CaesarCode
+{
+    public class CaesarCodeClass
+    {
+        public string Encrypt(string textToEncrypt, int shift)
+        {
+            string enctypyText = "";
+            char minChar=' ';
+            char maxChar=' ';
+            bool encryptCharFlag = false;
+            int loopShift = 0;
+            foreach (char character in textToEncrypt)
+            {
+                if (character >= 'A' && character <= 'Z')
+                {
+                    minChar = 'A';
+                    maxChar = 'Z';
+                    encryptCharFlag = true;
+                    loopShift = shift % (maxChar - minChar);
+                }
+                   
+                if (character >= 'a' && character <= 'z')
+                {
+                    minChar = 'a';
+                    maxChar = 'z';
+                    encryptCharFlag = true;
+                    loopShift = shift % (maxChar - minChar);
+                }
+                if (character >= '0' && character <= '9')
+                {
+                    minChar = '0';
+                    maxChar = '9';
+                    encryptCharFlag = true;
+                    loopShift = shift % (maxChar - minChar);
+                }
+
+                char encryptChar = character;
+                if (encryptCharFlag)
+                {
+                    encryptChar = (char)(character + loopShift);
+                    if (encryptChar > maxChar)
+                        encryptChar = (char)(encryptChar - (maxChar - minChar + 1));
+                    if (encryptChar < minChar)
+                        encryptChar = (char)(encryptChar + (maxChar - minChar + 1));
+
+                    encryptCharFlag = false;
+                    loopShift = shift;
+                }
+                enctypyText += encryptChar;
+            }
+            return enctypyText;
+        }
+
+        public string Decrypt(string textToDecrypt, int shift)
+        {
+            return Encrypt(textToDecrypt, shift);
+        }
+
+    }
+}
---
+++ Pierwszy projekt/CaesarCodeConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using CaesarCode;
+using System;
+
+namespace CaesarCodeConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj tekst do szyfrowania");
+            string text = Console.ReadLine();
+
+            int shift=0;
+            Console.WriteLine("Podaj przesunięcie");
+            int.TryParse(Console.ReadLine(), out shift);
+
+            string encryptText = new CaesarCodeClass().Encrypt(text, shift);
+            Console.WriteLine("Zaszyfrowany tekst:");
+            Console.WriteLine(encryptText);
+
+
+            string decrypText = new CaesarCodeClass().Decrypt(encryptText, -shift);
+            Console.WriteLine("Odszyfrowany tekst");
+            Console.WriteLine(decrypText);
+        }
+    }
+}
