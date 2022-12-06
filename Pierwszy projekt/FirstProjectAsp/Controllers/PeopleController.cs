c23cbb492826e0660753226b3303823d44340a95
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-12-06 13:15:30 +0100

    koniec lekcji

--- Pierwszy projekt/FirstProjectAsp/Controllers/PeopleController.cs
+++ Pierwszy projekt/FirstProjectAsp/Controllers/PeopleController.cs
@@ -34,7 +34,7 @@ namespace FirstProjectAsp.Controllers
             });
             peopleDbContext.SaveChanges();
 
-            return View("Index");
+            return RedirectToAction("ShowAllPersons");
         }
 
         public IActionResult ShowAllPersons()
@@ -52,26 +52,82 @@ namespace FirstProjectAsp.Controllers
             return View(allPersonsList);
         }
 
-
-        //-----------------------------------------------------------------------------
-
-        public IActionResult ViewPerson()
+        [HttpGet]
+        public IActionResult EditPerson(int id)
         {
-            PersonDto person = new PersonDto()
-            {
-                Name="Jan",
-                Surname = "Kowalski",
-                Age = 19
-            };
-
-            return View(person);
+            PersonDto personDto = peopleDbContext.Persons
+                .Where(x => x.Id == id)
+                .Select(x => new PersonDto() 
+                {
+                Id = x.Id,
+                Name = x.Name,
+                Surname = x.Surname,
+                Age = x.Age
+                })
+                .FirstOrDefault();
+            return View(personDto);
         }
 
-        //[HttpPost]
-        public IActionResult EditPerson([FromBody]PersonDto person)
+        [HttpPost]
+        public IActionResult EditPerson(PersonDto personDto)
         {
-            
-            return View(person);
+            Person person = peopleDbContext.Persons.FirstOrDefault(x => x.Id == personDto.Id);
+
+            if (person == null)
+                return NotFound();
+
+            person.Name = personDto.Name;
+            person.Surname = personDto.Surname;
+            person.Age = personDto.Age;
+            peopleDbContext.SaveChanges();
+
+            return RedirectToAction("ShowAllPersons");
+        }
+
+        public IActionResult DeletePerson(int id)
+        {
+            Person person = peopleDbContext.Persons.FirstOrDefault(x => x.Id == id);
+
+            if (person == null)
+                return NotFound();
+
+            peopleDbContext.Persons.Remove(person);
+            peopleDbContext.SaveChanges();
+
+            return RedirectToAction("ShowAllPersons");
+        }
+
+        [HttpGet]
+        public IActionResult SearchPerson([FromQuery]string name, [FromQuery] string surname)
+        {
+            List<PersonDto> newList = peopleDbContext.Persons
+                .Where(x => string.IsNullOrWhiteSpace(name) || x.Name.Contains(name))
+                .Where(x => string.IsNullOrWhiteSpace(surname) || x.Surname.Contains(surname))
+                .Select(x => new PersonDto()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Surname = x.Surname,
+                    Age = x.Age
+                })
+                .ToList();
+
+            /*
+            var newCollection = peopleDbContext.Persons.AsQueryable();
+            if (!string.IsNullOrWhiteSpace(name))
+                newCollection = newCollection.Where(x => x.Name.Contains(name));
+
+            List<PersonDto> newList = newCollection.Select(x => new PersonDto()
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Surname = x.Surname,
+                Age = x.Age
+            })
+            .ToList();
+            */
+
+            return View(newList);
         }
     }
 }
